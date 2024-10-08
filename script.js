d6fd348e92400d0df9db3db4154215f6ed3b9a0c
katsudon-tabetai <131325371+katsudon-tabetai@users.noreply.github.com>
2024-10-08 10:24:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    document.body.classList.toggle('black');
+});
+
+//functionを使った書き方
+// function change() {
+//   document.body.classList.toggle('black');
+// }
+// btn.addEventListener('click', change);
